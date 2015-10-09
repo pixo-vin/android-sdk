df76cefab759ca33d32f6140826f8567459850b1
Ben Young <ben@pixotech.com>
2015-10-09 11:41:31 -0500

    Add AndroidScanLib and SampleApplication

---
+++ SampleApplication/app/src/main/java/com/pixotech/sample/ScanActivity.java
@@ -0,0 +1,26 @@
+package com.pixotech.sample;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.pixotech.android.scanner.library.ScannerActivity;
+
+
+public class ScanActivity extends ScannerActivity {
+
+    private TextView vinTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addLayout(R.layout.activity_scan);
+        vinTextView = (TextView)findViewById(R.id.vinTextView);
+    }
+
+    @Override
+    public void handleDecode(String s) {
+        vibrate();
+        vinTextView.setText(s);
+    }
+}
